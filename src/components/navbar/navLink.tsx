28316a5c44811632ff1cc7ebef32f8c8bac8d130
Benoit Fardoux <fardouxbenoit@gmail.com>
2025-05-01 23:01:00 +0200

    changement logo

--- src/components/navbar/navLink.tsx
+++ src/components/navbar/navLink.tsx
@@ -5,9 +5,11 @@ type props  = {
 }
 const NavbarLink = (props : props) => {
     return (
+        <a href={props.link} className="w-full h-full">
         <div className="hover:bg-primary w-full rounded transition duration-150 ease-in-out flex justify-center items-center text-xl">
             <li className="m-2">{props.text}</li>
         </div>
+        </a>
 
     )
 }
