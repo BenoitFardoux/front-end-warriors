ce4c98cf2ca35481fc327a1d5267e2a5b23e61a7
Benoit Fardoux <fardouxbenoit@gmail.com>
2025-05-01 22:51:31 +0200

    ajout padding et agrandissement texte

--- src/components/navbar/navLink.tsx
+++ src/components/navbar/navLink.tsx
@@ -5,8 +5,8 @@ type props  = {
 }
 const NavbarLink = (props : props) => {
     return (
-        <div className="hover:bg-primary w-full rounded transition duration-150 ease-in-out flex justify-center items-center">
-            <li>{props.text}</li>
+        <div className="hover:bg-primary w-full rounded transition duration-150 ease-in-out flex justify-center items-center text-xl">
+            <li className="m-2">{props.text}</li>
         </div>
 
     )
