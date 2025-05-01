7d3245618f9cfa0fc1e4c8d7a8ce695b270b9f8d
Benoit Fardoux <fardouxbenoit@gmail.com>
2025-05-01 22:30:46 +0200

    suppression logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from '../assets/logo_warriors.png'
 import NavbarLink from './navbar/navLink';
 const Header : React.FC= () => {
   return (
     <header className=' border-b-2 border-primary'>
       <nav className='w-full'>
         <ul className='flex justify-evenly items-stretch'>
-          <img src={logo} className='max-w-md max-h-16 object-contain'></img>
           <NavbarLink text='Accueil' link='/' />
           <NavbarLink text='À propos'  link='/about'/>
           <NavbarLink text='Tournois' link='/tournois' />
