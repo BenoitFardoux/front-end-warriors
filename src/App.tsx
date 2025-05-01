7660a380cbac98ec1018ceb9a3a8074d71bd2a64
Benoit Fardoux <86200150+BenoitFardoux@users.noreply.github.com>
2025-05-01 22:40:14 +0200

    Feat/reparation nav bar (#1)
    * suppression coquille
    
    * suppression logo

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,6 @@ const App : React.FC = () =>  {
   return (
     <>
     <Header></Header>
-    <h1 className="text-3xl font-bold underline">
-a
-    </h1>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
     </>
   );
 }
